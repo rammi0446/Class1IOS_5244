6096634374b9ff3d58b20c6097d94a5776feba6f
Ramandeep <rammmi.0446@gmail.com>
2019-06-05 08:28:36 -0400

    initial commit

---
+++ Class1/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Class1
+//
+//  Created by MacStudent on 2019-06-05.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func btn(_ sender: Any) {
+    }
+    @IBOutlet weak var txt: UITextField!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
